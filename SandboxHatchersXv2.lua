f02494fc72abb1fb53dfb5897de1a1560513672d
LostThisGame <123263996+LostThisGame@users.noreply.github.com>
2023-01-21 19:39:41 +0100

    Update SandboxHatchersXv2.lua

--- SandboxHatchersXv2.lua
+++ SandboxHatchersXv2.lua
@@ -1,6 +1,7 @@
 core = loadstring(game:HttpGet('https://raw.githubusercontent.com/brownfieldd00/core/main/core.lua'))()
 local Collectables = workspace.CollectablesFolder
 local Collectables2 = workspace.CollectablesFolderW2
+local Collectables3 = workspace.CollectablesFolderW3
 while true do
     task.wait(.5)
     for i, v in next, Collectables:GetChildren() do
@@ -12,6 +13,14 @@ while true do
         end
     end
     for i, v in next, Collectables2:GetChildren() do
+        if v.Name == 'Coin' and v.Transparency ~= 1 then
+            core:gPlayer().Character.HumanoidRootPart.CFrame = CFrame.new(v.Position)
+            task.wait()
+            firetouchinterest(core:gPlayer().Character.HumanoidRootPart, v, 0)
+            firetouchinterest(core:gPlayer().Character.HumanoidRootPart, v, 1)
+        end
+    end
+    for i, v in next, Collectables3:GetChildren() do
         if v.Name == 'Coin' and v.Transparency ~= 1 then
             core:gPlayer().Character.HumanoidRootPart.CFrame = CFrame.new(v.Position)
             task.wait()
